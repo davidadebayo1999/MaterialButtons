7c9e4bbab8d1586282687637c74a5fad87c55a37
Adebayo Osunbiyi <repo@itandtsolutions.net>
2018-02-02 10:03:49 +0100

    last commit

--- app/src/main/java/app/kbuild/com/materialbuttons/MainActivity.java
+++ app/src/main/java/app/kbuild/com/materialbuttons/MainActivity.java
@@ -1,13 +1,36 @@
 package app.kbuild.com.materialbuttons;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    AppCompatButton appCompatButton;
+    FloatingActionButton floatingActionButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        floatingActionButton=(FloatingActionButton)findViewById(R.id.fab_btn);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Raised Button",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        appCompatButton=(AppCompatButton)findViewById(R.id.raise_btn);
+        appCompatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Raised Button",Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
